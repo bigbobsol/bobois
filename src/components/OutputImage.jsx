119796370312a2edf581508152178d973679c134
bigbobsol <asgerhelmuth@gmail.com>
2023-12-29 12:53:41 +0100

    Updated Image
    (The right place this time..)

--- src/components/OutputImage.jsx
+++ src/components/OutputImage.jsx
@@ -5,7 +5,7 @@ function ImageSection() {
                     className='output-image' 
                     id='output' 
                     alt='outputImage'
-                    src='https://media.discordapp.net/attachments/1185256400906104923/1185850181221105775/anon-quekz.png'
+                    src='https://media.discordapp.net/attachments/1185256400906104923/1190260080487186433/Datcat_Showcase.png'
                     draggable='false'/>
                 </div>
 )
