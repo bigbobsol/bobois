eed81db4af78c8e24ade85b8af2fd5310218b4ee
bigbobsol <asgerhelmuth@gmail.com>
2023-12-29 13:35:53 +0100

    Updated Image

--- src/components/OutputImage.jsx
+++ src/components/OutputImage.jsx
@@ -1,13 +1,13 @@
 function ImageSection() {
     return (
         <div className='image_right'>
-                    <img 
-                    className='output-image' 
-                    id='output' 
-                    alt='outputImage'
-                    src='https://media.discordapp.net/attachments/1185256400906104923/1190263461817634856/Datcat_Showcase.png'
-                    draggable='false'/>
-                </div>
+            <img 
+                className='output-image' 
+                id='output' 
+                alt='outputImage'
+                src='https://media.discordapp.net/attachments/1185256400906104923/1190263461817634856/Datcat_Showcase.png'
+                draggable='false'/>
+        </div>
 )
 }
 
