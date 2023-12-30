76126292647bcc65a9441bc7cdb17dde3430b53b
bigbobsol <asgerhelmuth@gmail.com>
2023-12-30 09:58:45 +0100

    Created Style Function

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -3,27 +3,37 @@ import { ImageGenerationScript } from './imageGenerationScript.js'
 function GenerateSection() {
     return (
         <div className='text_lower'>
-                            <input 
-                                type='number'
-                                className='input' 
-                                placeholder=' EXAMPLE: #659'
-                                id='input-nftId' 
-                                autoComplete='off'
-                            />
-                            <br/>
-                            <a 
-                                href="https://media.discordapp.net/attachments/1185256400906104923/1190263461817634856/Datcat_Showcase.png" 
-                                id="link"
-                                alt='outputImage'>
-                            </a>
-                            <button
-                            id='submit_button' 
-                            className='submit_button'
-                            onClick={() => {
-                                ImageGenerationScript()
-                            }}
-                            >GENERATE</button>
-                        <p className='outputText hide' id='text-output'>TEST</p>
+            <form 
+            className='form'
+            onSubmit={ImageGenerationScript}>
+                <input 
+                    type='number'
+                    className='input' 
+                    placeholder=' EXAMPLE: #659'
+                    id='input-nftId' 
+                    autoComplete='off'
+                    />
+                <br/>
+                <select
+                className='input'
+                defaultValue=""
+                id='chooseStyle'>
+                    <option value="">CHOOSE STYLE</option>
+                    <option value="transparent">TRANSPARENT</option>
+                    <option value="background">BACKGROUND</option>
+                </select>
+                <a 
+                    href="https://media.discordapp.net/attachments/1185256400906104923/1190263461817634856/Datcat_Showcase.png" 
+                    id="link"
+                    alt='outputImage'>
+                </a>
+                <button
+                    type='submit'
+                    id='submit_button' 
+                    className='submit_button'
+                    >GENERATE</button>
+                    <p className='outputText hide' id='text-output'>TEST</p>
+            </form>
                     </div>
 )
 }
--- src/components/imageGenerationScript.js
+++ src/components/imageGenerationScript.js
@@ -1,21 +1,30 @@
-export function ImageGenerationScript() {
+export function ImageGenerationScript(e) {
+      e.preventDefault();
         const nftID = document.querySelector("#input-nftId");
         const changeImage = document.querySelector("#output");
+        const chosenStyle = document.querySelector("#chooseStyle");
         const textOutput = document.querySelector("#text-output");
         const updateButton = document.querySelector("#submit_button");
         const link = document.querySelector("#link")
       
           console.log(nftID.value.length)
+          console.log(chosenStyle.value)
           const postData = {
-            "nft_id": nftID.value
+            "nft_id": nftID.value,
+            "style": chosenStyle.value
           };
           if (nftID.value.length === 0 || nftID.value > 4999) {
             textOutput.classList.remove("hide")
             console.log("Submission was empty. Please input your datcat ID")
             document.querySelector("#text-output").textContent = "PLEASE INPUT A VALID DATCAT ID"
           };
+          if (chosenStyle.value === "") {
+            textOutput.classList.remove("hide")
+            console.log("Submission was empty. Please input your datcat ID")
+            document.querySelector("#text-output").textContent = "PLEASE FILL ALL THE BOXES"
+          };
 
-          if (nftID.value.length > 0 && nftID.value <= 4999) {
+          if (nftID.value.length > 0 && nftID.value <= 4999 && chosenStyle.value.length > 1) {
             textOutput.classList.remove("hide")
             document.querySelector("#text-output").textContent = "LOADING..."
 
